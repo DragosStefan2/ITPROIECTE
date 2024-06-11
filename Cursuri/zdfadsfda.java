92bd4013087260e931fcfc116b8688a9137583a0
DragosStefan2 <dragos.stefan2003@yahoo.com>
2024-06-11 20:06:00 +0300

    update

---
+++ Cursuri/zdfadsfda.java
@@ -0,0 +1,26 @@
+package Cursuri;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class zdfadsfda {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob");
+        printFirstItem(names);
+    }
+
+    public static <T> void printFirstItem(List<T> list) {
+        System.out.println(list.get(0));
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
